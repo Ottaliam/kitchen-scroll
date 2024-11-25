4c58772dfec217ec9e5ea6138df27171c5bdd538
Ottaliam <ottaliam@gmail.com>
2024-11-25 20:28:20 +0000

    Add basic Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import reactLogo from '../assets/react.svg'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoContainer}>
+        <img src={reactLogo} alt="Recipe Scrolls Logo"/>
+        <h1>Recipe Scrolls</h1>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/recipes">Recipes</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+};
+
+export default Header;
