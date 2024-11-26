1f2f58fb4298eea9e7b314c35a2e7db6f81f3965
Ottaliam <ottaliam@gmail.com>
2024-11-26 18:35:32 +0000

    Add Router and remove App.jsx for multiple pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import {BrowserRouter, Route, Routes} from "react-router";
+import Home from "./pages/Home/Home.jsx";
+
+import 'normalize.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element = {<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
