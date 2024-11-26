1f31ade3e12b5afdcc3f76182a3ed03fcdd4e62a
Ottaliam <ottaliam@gmail.com>
2024-11-26 06:38:57 +0000

    Add skip to main

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,11 +3,14 @@ import CategoryCardList from "./components/CategoryCardList.jsx";
 import SearchBox from "./components/SearchBox.jsx";
 import Footer from "../../components/Footer.jsx";
 
+import styles from './Home.module.css';
+
 const Home = () => {
   return (
     <>
+      <a href="/#main" className={styles.skipMain}>Skip to main content</a>
       <Header />
-      <main>
+      <main id="main">
         <CategoryCardList />
         <SearchBox />
       </main>
