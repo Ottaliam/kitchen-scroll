163869da65eb2f4dfca79c3fcd32a2abb591b567
Ottaliam <ottaliam@gmail.com>
2024-12-08 19:40:56 +0000

    Add AboutPage

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import RecipePage from "./pages/RecipePage/RecipePage.jsx";
 import RecipeListPage from "./pages/RecipeListPage/RecipeListPage.jsx";
+import AboutPage from "./pages/AboutPage/AboutPage.jsx";
 
 import recipes from "./utils/recipes.js";
 import 'normalize.css';
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route index element = {<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
         <Route path="list">
           <Route index element={<RecipeListPage items={recipes} />} />
           <Route path="breakfast" element={<RecipeListPage items={recipes.filter(recipe => recipe.category === 'breakfast')} />} />
---
+++ src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,42 @@
+import Header from "../../components/Header.jsx";
+import Footer from "../../components/Footer.jsx";
+
+import styles from './AboutPage.module.css';
+
+const AboutPage = () => {
+  return (
+    <>
+      <Header/>
+      <main id="main" className={styles.main}>
+        <h2>About This Site</h2>
+        <p>
+          This site collects pieces from the Elder Scroll about recipes that satisfied the hunger of Alduin, the First Dragon.
+        </p>
+        <p>
+          Thanks to the last Dragonborn, the World Eater had been put to a permanent end.
+          But these prophecies may prove useful until the end of time comes.
+        </p>
+
+        <article>
+          <h2>
+            The Elder Scroll
+          </h2>
+          <div className={styles.elderScroll}>
+            <p>
+              And the Scrolls have foretold, of black wings in the cold, That when brothers wage war come unfurled!
+            </p>
+            <p>
+              Alduin, Bane of Kings, ancient shadow unbound, With a hunger to swallow the world!
+            </p>
+            <p>
+              - Song of the Dragonborn.
+            </p>
+          </div>
+        </article>
+      </main>
+      <Footer/>
+    </>
+  );
+};
+
+export default AboutPage;
